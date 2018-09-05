942c6ff4548d1ea67a2ad1b8203287e91f71a678
Saksham Sneh Mandal <sakshamsneh@gmail.com>
2018-09-05 16:18:36 +0530

    Update mongodb1.py

--- mongodb1.py
+++ mongodb1.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient
 import pandas as pd
+#Offline db:
 #client = MongoClient()
 #db = client.ibm1
+filename=""
 client= MongoClient("mongodb+srv://sakshamsneh:SAKsneh098#@ibm1-1tqxu.mongodb.net/test?retryWrites=true")
 db = client.test
 dataflow = db.dataflow
-df = pd.read_csv("C:/Users/SAKSHIM/Documents/IBMProj/UNSW-NB15_4_1.csv")
+df = pd.read_csv(filename)
 records_ = df.to_dict(orient = 'records')
 result = db.dataflow.insert_many(records_ )
 print(db.dataflow.find().count())
